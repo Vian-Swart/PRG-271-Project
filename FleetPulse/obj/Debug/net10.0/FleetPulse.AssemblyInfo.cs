ed3c53bc165143f9ac864c89322616e79a044eb7
Vian Swart <vian@snappycleaning.co.za>
2026-08-17 18:11:29 +0000

    Testing

--- FleetPulse/obj/Debug/net10.0/FleetPulse.AssemblyInfo.cs
+++ FleetPulse/obj/Debug/net10.0/FleetPulse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetPulse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f096a8d15cebd85eb1009be4d17175bfc8dd89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ec6408cfc7690f946fbe93d8aaed76df209cdd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetPulse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetPulse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
